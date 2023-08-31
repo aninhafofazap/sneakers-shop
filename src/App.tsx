9f1d99b2749b6a6d40a8db5cdd06723da073d210
Ana Victoria <anavictoriagomes1810@gmail.com>
2023-08-30 23:24:03 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import logo from "./assets/images/logo.svg";
+import menu from "./assets/images/icon-menu.svg";
+import cart from "./assets/images/icon-cart.svg";
+import { CgProfile } from "react-icons/cg";
+import close from "./assets/images/icon-close.svg";
 
 function App() {
+  // Usei o useState para criar um estado chamado "open", com o valor inicial de "false"
+  // depois criei o setClose pra poder atualizar o valor do estado
+  const [open, setClose] = useState(false);
+
+  const selectMenu = () => {
+    // criei a funçao pra poder chamar quando o icone do menu for clicado.
+    // o setClose e pra quando o valor for clicado inverter o valor de true pra false ou false pra true
+    setClose(!open);
+    console.log("estou aqui");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <nav className="navbar">
+        <div className="menu-icon" onClick={selectMenu}>
+          <img src={menu} alt="" />
+        </div>
+        <div className="logo">
+          <img src={logo} alt="Logo da loja" />
+        </div>
+        <div className="cart">
+          <img src={cart} alt="Carrinho da loja" />
+        </div>
+        <div className="profile">
+          <CgProfile />
+        </div>
+      </nav>
+      {open && (
+        <>
+          <div
+            className={`${open && "active-background"} dark-background`}
+          ></div>
+          <div className={`${open && "menu-active"} menu-options`}>
+            <img
+              src={close}
+              alt=""
+              className="icon-close"
+              onClick={selectMenu}
+            />
+            <ul>
+              <li>Collections</li>
+              <li>Men</li>
+              <li>Women</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   );
 }
